3c0231c2911b956d2d58c16b681751dcf8d1919d
Jake Stothard <stothardj@gmail.com>
2013-02-16 22:20:54 -0800

    Initial commit. Have index and overlay working separatly

---
+++ js/update.js
@@ -0,0 +1,37 @@
+var handle = null;
+var old = null;
+
+function htmlEscape(str) {
+    return String(str)
+        .replace(/&/g, '&amp;')
+        .replace(/"/g, '&quot;')
+        .replace(/'/g, '&#39;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;');
+}
+
+function updateBox() {
+    var ugly = document.getElementById("ugly");
+    var pretty = document.getElementById("pretty");
+    var hb = document.getElementById("textCopy");
+    if( ugly.value !== old ) {
+	console.log( ugly.value );
+	var escaped = htmlEscape(ugly.value);
+	pretty.innerHTML = escaped;
+	if( hb !== null ) {
+	    hb.innerHTML = escaped;
+	}
+	// For some odd reason prettyPrintOne puts everything on one line in chromium
+	prettyPrint();
+	old = ugly.value;
+    }
+}
+
+function update() {
+    // Throw away an update if it happened really recently
+    // Trust me, this makes it actually feel more responsive
+    if( handle !== null ) {
+    	clearTimeout( handle );
+    }
+    handle = setTimeout( "updateBox()", 250 );
+}
